0a81b09f88796490ac92649cefb1d9ffb7269ca7
hardik gupta <139064895+hardik-2410@users.noreply.github.com>
2024-04-14 14:16:09 +0530

    updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://real-estate-full-stack-backend-p1vpbmr4f.vercel.app///api",
+  baseURL: "https://real-estate-full-stack-omega.vercel.app//api",
 });
 
 export const getAllProperties = async () => {
