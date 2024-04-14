ed40938bab0a971ea7f1cd53d446de03e7c47309
hardik gupta <139064895+hardik-2410@users.noreply.github.com>
2024-04-14 16:23:13 +0530

    mkc

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-0i5hl0ctttkjzb6f.us.auth0.com"
      clientId="nnVphrmCPnadbUgQnb2dncewwftdIPfT"
      authorizationParams={{
-      redirect_uri: "real-estate-full-stack-ians.vercel.app"
+      redirect_uri: "https://real-estate-full-stack-ians-fa43wgk2n-hardiks-projects-05c01778.vercel.app"
      }}
      audience="http://localhost:8000/api"
      scope="openid profile email"
